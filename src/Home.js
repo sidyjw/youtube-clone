6dd57b3a4317155bce7e0fd4130fc791b61a52ff
Sidiney Silva <sidineysilva1@gmail.com>
2020-08-04 21:01:59 -0300

    Adicionei o grid

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,33 @@
 import React from "react";
 import DrawerYt from "./DrawerYt";
 import Header from "./Header";
-import { Grid } from "@material-ui/core";
+import { Grid, Box } from "@material-ui/core";
+import VideoGrid from "./VideoGrid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  contentTop: {
+    marginTop: theme.spacing(8),
+  },
+}));
+
 export default function Home() {
+  const classes = useStyles();
   return (
     <>
-      <Grid container>
-        <Grid item xs={2}>
-          <Header />
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item>
-          <DrawerYt />
-        </Grid>
-        <Grid item>
-          <div>eeee</div>
-        </Grid>
-      </Grid>
+      <Header />
+
+      <Box
+        height="100%"
+        width="100%"
+        display="flex"
+        className={classes.contentTop}
+      >
+        <DrawerYt />
+        <Box p={4}>
+          <VideoGrid />
+        </Box>
+      </Box>
     </>
   );
 }
